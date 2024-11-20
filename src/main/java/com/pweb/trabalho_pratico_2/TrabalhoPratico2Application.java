284d9b223113e85bde0d37068e3601bfd9ab7c47
Maria Rita <maria@mariarita.lustosa>
2024-11-20 13:07:39 -0300

    criação do projeto

---
+++ src/main/java/com/pweb/trabalho_pratico_2/TrabalhoPratico2Application.java
@@ -0,0 +1,13 @@
+package com.pweb.trabalho_pratico_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabalhoPratico2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabalhoPratico2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/pweb/trabalho_pratico_2/TrabalhoPratico2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pweb.trabalho_pratico_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrabalhoPratico2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
