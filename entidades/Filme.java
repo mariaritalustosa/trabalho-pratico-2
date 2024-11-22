6aa5c59c7e5b7b313e7671c1780a98da31c82a73
Maria Rita <maria@mariarita.lustosa>
2024-11-22 12:54:58 -0300

    último mapeamento de entidade e criação dos getters e setters

--- entidades/Filme.java
+++ entidades/Filme.java
@@ -1,3 +1,4 @@
+import java.util.List;
 import jakarta.persistence.*;
 
 @Entity
@@ -15,4 +16,59 @@ public class Filme {
     @ManyToOne
     @JoinColumn(name = "distribuidora_id")
     private Distribuidora distribuidora;
+
+    @ManyToMany
+    @JoinTable(name = "filme_genero",
+    joinColumns = @JoinColumn(name="filme_id"),
+    inverseJoinColumns = @JoinColumn(name= "genero_id"))
+    private List<Genero> genero;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public int getAnoLancamento() {
+        return anoLancamento;
+    }
+
+    public void setAnoLancamento(int anoLancamento) {
+        this.anoLancamento = anoLancamento;
+    }
+
+    public Diretor getDiretor() {
+        return diretor;
+    }
+
+    public void setDiretor(Diretor diretor) {
+        this.diretor = diretor;
+    }
+
+    public Distribuidora getDistribuidora() {
+        return distribuidora;
+    }
+
+    public void setDistribuidora(Distribuidora distribuidora) {
+        this.distribuidora = distribuidora;
+    }
+
+    public List<Genero> getGenero() {
+        return genero;
+    }
+
+    public void setGenero(List<Genero> genero) {
+        this.genero = genero;
+    }
 }
+
