e0462a012c59eac34badfca87d32c6c290962894
Maria Rita <maria@mariarita.lustosa>
2024-11-22 13:24:49 -0300

    mapeamento dos relacionamentos na entidade genero

--- entidades/Genero.java
+++ entidades/Genero.java
@@ -1,9 +1,16 @@
+import java.util.List;
+
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 
 public class Genero {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
+
+    @ManyToMany(mappedBy = "generos")
+    private List<Filme> filmes;
+    
 }
