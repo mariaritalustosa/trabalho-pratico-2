6cd6243d48030176f21603b471c11f3658198a0c
Maria Rita <maria@mariarita.lustosa>
2024-11-22 13:14:06 -0300

    implementação do construtor na entidade diretor e criação dos getters e setters

--- entidades/Diretor.java
+++ entidades/Diretor.java
@@ -13,6 +13,34 @@ public class Diretor {
     @JoinColumn(name="filme_id")
     private Filme filme;
 
+    public Diretor(){}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Filme getFilme() {
+        return filme;
+    }
+
+    public void setFilme(Filme filme) {
+        this.filme = filme;
+    }
+
     
+
+
 
 }
