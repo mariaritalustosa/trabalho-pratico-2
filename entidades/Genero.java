da7e73aa720ad960f6aaeae360b7532cb98a63c8
Maria Rita <maria@mariarita.lustosa>
2024-11-22 13:26:23 -0300

    criação dos getters e setters da entidade genero

--- entidades/Genero.java
+++ entidades/Genero.java
@@ -12,5 +12,30 @@ public class Genero {
 
     @ManyToMany(mappedBy = "generos")
     private List<Filme> filmes;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public List<Filme> getFilmes() {
+        return filmes;
+    }
+
+    public void setFilmes(List<Filme> filmes) {
+        this.filmes = filmes;
+    }
+
     
 }
